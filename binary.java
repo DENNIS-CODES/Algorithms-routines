8f448bbb2de5026a5551e11c6df12ab970708bbe
DENNIS MWANGI <dennistrevor06@gmail.com>
2021-03-19 06:17:30 +0000

    returns position of the element

---
+++ binary.java
@@ -0,0 +1,17 @@
+/*The binarySearch() method of the Java collections framework searches for the specified element. It returns the position of the element in the specified collections*/
+import java.util.Collections;
+import java.util.ArrayList;
+
+class Main {
+    public static void main(String[] args) {
+        // Creating an ArrayList
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+        // Using binarySearch()
+        int pos = Collections.binarySearch(numbers, 3);
+        System.out.println("The position of 3 is " + pos);
+    }
+}
